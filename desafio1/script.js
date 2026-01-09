86475c5b6492fe7b26f8e88cd9fdf8f5b3cebc0c
TarcisioJr-dev <tarcisioalgoritmo@gmail.com>
2026-01-09 17:56:22 -0300

    atualização no if de entrada

--- desafio1/script.js
+++ desafio1/script.js
@@ -1,25 +1,27 @@
 let nome = "Tarcísio-Jr"
-var xp = 678
-let nivel = ""
+var xp = 234234
+let nivel
 
-if (xp < 0) {
+if (!(xp > 0)) {
     console.log("[ERRO] Informe um valor de experiência válido.")
-}else if(xp <= 1000){
-    nivel = "Ferro"
-}else if(xp <= 2000){
-    nivel = "Bronze"
-}else if(xp <= 5000){
-    nivel = "Prata"
-}else if(xp <= 7000){
-    nivel = "Ouro"
-}else if(xp <= 8000){
-    nivel = "Platina"
-}else if(xp <= 9000){
-    nivel = "Ascendente"
-}else if(xp <= 10000){
-    nivel = "Imortal"
-}else{
-    nivel = "Radiante"
+}else {
+    if(xp <= 1000){
+        nivel = "Ferro"
+    }else if(xp <= 2000){
+        nivel = "Bronze"
+    }else if(xp <= 5000){
+        nivel = "Prata"
+    }else if(xp <= 7000){
+        nivel = "Ouro"
+    }else if(xp <= 8000){
+        nivel = "Platina"
+    }else if(xp <= 9000){
+        nivel = "Ascendente"
+    }else if(xp <= 10000){
+        nivel = "Imortal"
+    }else{
+        nivel = "Radiante"
+    }
+    console.log("O Herói de nome "+ nome +" está no nível de "+ nivel+" com "+ xp+" de xp.")
 }
-console.log("O Herói de nome "+ nome +" está no nível de "+ nivel+".")
                                     
